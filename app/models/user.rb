5103965b19d4a1167cd58b88b513fccc4b201312
lanak1301 <musiconstellation@gmail.com>
2015-08-07 07:50:12 -0400

    made user dependent destroy on recipes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  has_many :recipes, foreign_key: "creator_id"
+  has_many :recipes, foreign_key: "creator_id", dependent: :destroy
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
